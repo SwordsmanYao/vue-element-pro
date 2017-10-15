28d258813937b5807ba72baae9d5aebc618167d7
SwordsmanYao <swordsman.yao@foxmail.com>
2017-10-15 22:36:59 +0800

    'init'

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+  /*引入全局css样式*/
+  @import "styles/index.scss";
+</style>
